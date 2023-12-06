de8f99c6a516487dc27512fba94b8095697a7ff5
RosenYanev <138884668+RosenYanev@users.noreply.github.com>
2023-12-06 19:17:34 +0200

    Update Playlist genre method fixed

--- RidePal/src/main/java/com/example/ridepal/models/dtos/PlaylistUpdateDto.java
+++ RidePal/src/main/java/com/example/ridepal/models/dtos/PlaylistUpdateDto.java
@@ -3,13 +3,15 @@ package com.example.ridepal.models.dtos;
 import com.example.ridepal.models.Genre;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class PlaylistUpdateDto {
 
-    @Size(min = 3, max = 20, message = "Title length should be between 3 and 20")
+/*    @Size(min = 3, max = 20, message = "Title length should be between 3 and 20")*/
     private String title;
-    private Set<Genre> genres;
+    private String genres;
     public PlaylistUpdateDto() {
     }
 
@@ -21,11 +23,12 @@ public class PlaylistUpdateDto {
         this.title = title;
     }
 
-    public Set<Genre> getGenres() {
+    public String getGenres() {
         return genres;
     }
 
-    public void setGenres(Set<Genre> genres) {
+    public void setGenres(String genres) {
         this.genres = genres;
     }
+
 }
